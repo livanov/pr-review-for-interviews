5967b4d6c4529ee2d68eb6ae9edd778222cc6514
ckojouharov <ckojouharov@gmail.com>
2020-08-28 11:15:10 +0300

    Changes 3

--- src/test/java/com/livanov/interview/InterviewApplicationTest.java
+++ src/test/java/com/livanov/interview/InterviewApplicationTest.java
@@ -6,18 +6,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.livanov.interview.repositories.PersonRepository;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
-import javax.transaction.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
